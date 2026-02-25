824095fe612cbbe53189b750a4519518b4da5611
ihor_romaniuk <iromaniuk@mycubes.nl>
2026-02-25 10:45:07 +0200

    changed

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 class Car:
     def __init__(self,
-                 comfort_class: int, clean_mark: int, brand: str) -> None:
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int, average_rating: float,
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -23,16 +27,16 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(
-            (car.comfort_class * (self.clean_power - car.clean_mark)
-             * self.average_rating / self.distance_from_city_center), 1)
+        return round((car.comfort_class * (self.clean_power - car.clean_mark)
+                      * self.average_rating
+                      / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
-        self.average_rating = round(((self.average_rating
+        self.average_rating = round((self.average_rating
                                      * self.count_of_ratings + rating)
-                                    / (self.count_of_ratings + 1)), 1)
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
