76cf54a5c692212e4064924acd4711946fb9a94e
ihor_romaniuk <iromaniuk@mycubes.nl>
2026-02-26 16:17:11 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str,
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -21,7 +25,9 @@ class CarWashStation:
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
-        for car in [car for car in cars if car.clean_mark < self.clean_power]:
+        cars_to_wash = [car for car in cars if
+                        car.clean_mark < self.clean_power]
+        for car in cars_to_wash:
             income += self.calculate_washing_price(car)
             self.wash_single_car(car)
         return round(income, 1)
