23e600ecbdee275385c3d41250923bf715454199
ihor_romaniuk <iromaniuk@mycubes.nl>
2026-02-26 16:19:14 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round((car.comfort_class * (self.clean_power - car.clean_mark)
-                      * self.average_rating
+        clean_required = self.clean_power - car.clean_mark
+        return round((car.comfort_class * clean_required * self.average_rating
                       / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: Car) -> None:
